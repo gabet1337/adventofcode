b3cfa79c417ca89595711655fbef5eb921e5d1f5
Peter Gabrielsen <peterg@cs.au.dk>
2015-12-18 10:36:26 +0100

    Day 16 and 17 fixed

--- day17.cpp
+++ day17.cpp
@@ -1,75 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
-#include <string>
+#include <algorithm>
+#include <bitset>
 using namespace std;
 
-// children: 3
-// cats: 7
-// samoyeds: 2
-// pomeranians: 3
-// akitas: 0
-// vizslas: 0
-// goldfish: 5
-// trees: 3
-// cars: 2
-// perfumes: 1
-
-
 int main() {
-  string skip;
-  string key;
-  int val;
-  vector<map<string,int> > kl(500,map<string,int>());
-
-  vector<int> possible;
-  for (int i = 0; i < 500; i++) possible.push_back(i);
-  for (int i = 0; i < 500; i++) {
-    cin >> skip >> skip >> key >> val >> skip;
-    key.pop_back();
-    kl[i][key] = val;
-    cin >> key >> val >> skip;
-    key.pop_back();
-    kl[i][key] = val;
-    cin >> key >> val;
-    key.pop_back();
-    kl[i][key] = val;
-    // for (auto x : kl[i]) cout << x.first << " " << x.second << endl;
+  vector<int> in;
+  int x;
+  while (cin >> x) in.push_back(x);
+  int res = 0;
+  int count[20];
+  for (int i = 0; i < 20; i++) count[i] = 0;
+  for (int i = 0; i < 1048576; i++) {
+    bitset<20> bs(i);
+    int sum = 0;
+    for (int j = 0; j < 20; j++) if (bs[j]) sum+=in[j];
+    if (sum == 150) {res++; count[bs.count()]++;}
   }
-  map<string,int> w;
-  w["children"] = 3;
-  w["cats"] = 7;
-  w["samoyeds"] = 2;
-  w["pomeranians"] = 3;
-  w["akitas"] = 0;
-  w["vizslas"] = 0;
-  w["goldfish"] = 5;
-  w["trees"] = 3;
-  w["cars"] = 2;
-  w["perfumes"] = 1;
-
-  for (int i = 0; i < 500; i++) {
-    bool pos = true;
-    for (auto x : kl[i]) {
-      if (x.first == "cats" || x.first == "trees") {
-	if (w[x.first] < x.second) continue;
-	else pos = false;
-	break;
-      } else if (x.first == "pomeranians" || x.first == "goldfish") {
-	if (w[x.first] > x.second) continue;
-	else pos = false;
-	break;
-      } else if (w[x.first] == x.second) continue;
-      else {
-	pos = false;
-	break;
-      }
-    }
-    if (pos) cout << i+1 << endl;
-
-  }
-
-
+  cout << res << endl;
+  for (int i = 0; i < 20; i++) if (count[i] != 0) { cout << count[i] << endl; break;}
   return 0;
 }
